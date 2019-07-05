818e3568b9cc5ca7e0653fbb811bba46dff7dd7d
Federico Michel <FMichel@brightstar.com>
2019-07-05 13:52:44 -0300

    dfasdaf

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+
+
+function createWindow () {
+  // Crea la ventana del navegador.
+  let win = new BrowserWindow({
+    width: 930,
+    height: 650,
+    webPreferences: {
+      nodeIntegration: true,
+      devTools: true
+    },
+    frame: false
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
